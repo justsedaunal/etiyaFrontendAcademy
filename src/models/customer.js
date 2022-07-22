5b55763fa3b39f8d5013ca64da8d253fe0dfcd01
Seda Ünal <sedaylmz06@outlook.com>
2022-07-22 19:07:29 +0300

    etiya camp end

---
+++ src/models/customer.js
@@ -0,0 +1,7 @@
+import User from "./user.js";
+export default class Customer extends User {
+  constructor(id, firstName, lastName, city, age, creditCardNumber) {
+    super(id, firstName, lastName, city, age);
+    this.creditCardNumber = creditCardNumber;
+  }
+}
